ef483a5b954648a91a09bdce7d0134a1d03e01a6
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-02-07 16:31:56 +0100

    card-matching functionality implemented. Hidding cards when matching, reducing user life when missing, and restarting the game when user life = 0.

--- src/app/level1/level1.page.ts
+++ src/app/level1/level1.page.ts
@@ -17,15 +17,14 @@ export class Level1Page implements OnInit {
   public selectCard1val = -1; // Selected card #1 value
   public selectCard2pos = -1; // Selected card #2 position
   public selectCard2val = -1; // Selected card #2 value
+  public selectOldPosix = -1; // Store old positions 
 
   public debugText = "Debug goes here";
 
   constructor() { }
 
   ngOnInit() {
-    this.populateCards();
-    this.shuffle(this.cardsArray);
-    this.shuffle(this.images);
+    this.restartGame();
   }
 
   // Function to populate cards array with position and value pairs from 0 to 6
@@ -43,12 +42,42 @@ export class Level1Page implements OnInit {
   }
 
   // Function to select a card
-  selectCard(pos, val) {
-    // Code to select de first card
-    this.selectCard1pos = pos;
-    this.selectCard1val = val;
+  selectCard(pos, val, i) {
+    let actOne = false;
 
-    this.debugText = "Pos: " + pos + " and val: " + val
+    // Code to select the second card
+    if (this.selectCard1pos > -1 && this.selectCard2pos == -1) {
+      this.selectCard2pos = pos;
+      this.selectCard2val = val;
+      actOne = true;
+    }
+
+    // Code to select the first card
+    if (this.selectCard1pos == -1 && !actOne) {
+      this.selectCard1pos = pos;
+      this.selectCard1val = val;
+      this.selectOldPosix = i;
+    }
+
+    // If we have both cards selected, check for match or fail
+    if (actOne && this.selectCard1pos > -1 && this.selectCard2pos > -1) {
+      setTimeout(() => {
+        // If the cards match, do this...
+        if (this.selectCard1val == this.selectCard2val) {
+          this.debugText = 'Cards match!';
+          this.cardsArray.splice(this.selectOldPosix, 1, { pos: this.selectOldPosix, val: -1 });
+          this.cardsArray.splice(i, 1, { pos: i, val: -1 });
+          this.resetSelects();
+        }
+        //Otherwise, take a life and reset
+        else {
+          this.debugText = 'Cards don\'t match';
+          this.userLife--;
+          this.resetSelects();
+          if (this.userLife <= 0) this.restartGame();
+        }
+      }, 1000);
+    }
   }
 
   // Function to shuffle an array
@@ -60,6 +89,15 @@ export class Level1Page implements OnInit {
       a[i - 1] = a[j];
       a[j] = x;
     }
+  }
+
+  // Function to restart the game
+  restartGame() {
+    this.userLife = 4;
+    this.resetSelects();
+    this.populateCards();
+    this.shuffle(this.cardsArray);
+    this.shuffle(this.images);
   }
 
   // Function to reset selected cards
